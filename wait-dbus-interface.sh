8f1833bed7716715632e6f1b844b04ca647e08d0
Aleksandr Mezin <mezin.alexander@gmail.com>
2021-05-24 01:01:29 +0600

    wait-dbus-interface.sh: remove -t option

--- wait-dbus-interface.sh
+++ wait-dbus-interface.sh
@@ -3,37 +3,35 @@
 set -e
 
 usage() {
-    >&2 echo "Usage: $0 -d destination -o object_path -i interface -t timeout"
+    >&2 echo "Usage: $0 -d destination -o object_path -i interface"
 }
 
 DESTINATION=
 OBJECT_PATH=
 INTERFACE=
-TIMEOUT=
 
-while getopts "d:o:i:t:h" opt; do
+while getopts "d:o:i:h" opt; do
     case $opt in
     d) DESTINATION="${OPTARG}";;
     o) OBJECT_PATH="${OPTARG}";;
     i) INTERFACE="${OPTARG}";;
-    t) TIMEOUT="${OPTARG}";;
     h) usage; exit 0;;
     *) usage; exit 1;;
     esac
 done
 
-if [[ -z "${DESTINATION}" ]] || [[ -z "${OBJECT_PATH}" ]] || [[ -z "${INTERFACE}" ]] || [[ -z "${TIMEOUT}" ]]; then
+if [[ -z "${DESTINATION}" ]] || [[ -z "${OBJECT_PATH}" ]] || [[ -z "${INTERFACE}" ]]; then
     usage
     exit 1
 fi
 
-for (( elapsed=0; elapsed<TIMEOUT; elapsed++ )); do
-    line_count="$(busctl introspect --user "${DESTINATION}" "${OBJECT_PATH}" "${INTERFACE}" | wc -l)"
-    if (( line_count > 1 )); then
-        exit 0
-    fi
-    sleep 1
-done
+gdbus wait --session "${DESTINATION}"
 
->&2 echo "Timed out"
-exit 1
+while : ; do
+    if gdbus introspect --session -d "${DESTINATION}" -o "${OBJECT_PATH}" | grep "interface ${INTERFACE} "; then
+        break
+    elif (( PIPESTATUS[0] )); then
+        exit "${PIPESTATUS[0]}"
+    fi
+    sleep 0.1
+done
