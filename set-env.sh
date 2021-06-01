2819e43eec77f0ea71e6a0440d4b6028f3d183f5
Aleksandr Mezin <mezin.alexander@gmail.com>
2021-06-01 08:29:54 +0600

    Update README and LICENSE

--- set-env.sh
+++ set-env.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# -------------------------------------------------------------------------------------- #
-# This is a little helper script which copies the environment of the bash process of the #
-# gnomeshell user. For many applications the environment variables like DISPLAY,         #
-# DBUS_SESSION_BUS_ADDRESS, or XDG_RUNTIME_DIR are required. While you set them all with #
-# a corresponding --env for podman exec, using this script is more convenient.           #
-#                                                                                        #
-# You can explore the effect with:                                                       #
-# podman exec --user gnomeshell $(podman ps -q -n 1) env                                 #
-# podman exec --user gnomeshell $(podman ps -q -n 1) /home/gnomeshell/set-env.sh env     #
-# -------------------------------------------------------------------------------------- #
-
-# Exit on error.
-set -e
-
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus
 
-# Run the given command.
 exec "$@"
